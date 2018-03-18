5946ec318b87feb7e30e7e09112ba59114ef5fdf
Dilyan Vasilev <dilyan.vasilev@Dilyans-MacBook-Pro.local>
2018-03-18 22:24:32 +0200

    feat: user seed

--- seed/user.js
+++ seed/user.js
@@ -1,16 +1,30 @@
 'use strict';
-
-module.exports = [
+let seeds = [
     {
         method: 'user.create',
         params: {
             username: 'admin',
             password: '123',
-            email: 'admin2@gmail.bg',
-            phone: '0873829733',
+            email: 'admin@admin.bg',
+            phone: '088888888',
             firstName: 'Admin',
             lastName: 'Admin',
-            middleName: '1111ddd'
+            middleName: 'Admin'
         }
     }
 ];
+for (var i = 0; i < 100; i++) {
+    seeds.push({
+        method: 'user.create',
+        params: {
+            username: 'admin' + i,
+            password: '123',
+            email: 'admin' + i + '@admin.bg',
+            phone: '0888888' + i,
+            firstName: 'Admin' + i,
+            lastName: 'Admin' + i,
+            middleName: 'Admin' + i
+        }
+    });
+}
+module.exports = seeds;
